1a172edee25c474d526e9693fbd4d750aad5de34
Posidonius <cbramdit@gmail.com>
2024-03-26 10:10:42 +0100

    WIP

--- src/UI/ContactPage.tsx
+++ src/UI/ContactPage.tsx
@@ -7,7 +7,7 @@ export function ContactPage() {
         <div>
           <h1>Contact us</h1>
           <p>We're just getting started, and would love to hear from you. How can we build the best possible product for you?</p>
-          <p>Don't hesitate to email us at <span className="underlined disappears">richard@chelsea.se</span>.</p>
+          <p>Don't hesitate to email us at <span className="underlined disappears">metis@chelsea.se</span>.</p>
         </div>
         <img src="/images/contact/oscar-nord-7WlEdxOy-QA-unsplash.jpg" alt="Stockholm"/>
       </main>
