eb77d183fdd57230fc17773ad897c7f57b204e0d
Christophe <cbramdit@gmail.com>
2024-04-12 14:23:56 +0200

    WIP

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { SpeedInsights } from "@vercel/speed-insights/react"
 import dayjs from "dayjs"
 import React from "react"
 import ReactDOM from "react-dom/client"
@@ -6,7 +5,6 @@ import ReactDOM from "react-dom/client"
 import "dayjs/locale/sv"
 import { App } from "./UI/App.tsx"
 import { getScrollbarWidth, isTouchDevice } from "./Util/BrowserUtils.ts"
-import { config } from "./config.ts"
 
 import "./main.scss"
 
@@ -19,6 +17,5 @@ document.body.style.setProperty("--scrollbar-width", `${getScrollbarWidth()}px`)
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App/>
-    {config.IS_PROD && <SpeedInsights />}
   </React.StrictMode>
 )
