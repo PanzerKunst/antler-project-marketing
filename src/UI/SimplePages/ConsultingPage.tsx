bbb5db02f6ce1976b5c7b9005e487751cdc7b2e7
Christophe <cbramdit@gmail.com>
2024-04-19 17:13:09 +0200

    Consulting page

--- src/UI/SimplePages/ConsultingPage.tsx
+++ src/UI/SimplePages/ConsultingPage.tsx
@@ -10,7 +10,7 @@ export function ConsultingPage() {
           <p>There are more than 200 GRC solutions available from €100 to €100 000 per month, with new options coming out every couple of months.</p>
           <p>Our extensive research can help you choose the right solution for your company by finding the right mix of homegrown and professional
             solutions for Risk, Compliance, Operations, Vendors, Contracts.</p>
-          <p>We'll find the right solution for you. Send us an email to <span className="underlined disappears">grace@chelsea.se</span> and let's
+          <p>We'll find the right solution for you. Send an email to <span className="underlined disappears">grace@chelsea.se</span> and let's
             setup a call!</p>
         </div>
         <img src="/images/consulting/consulting-desktop.jpg" alt="Consultancy" className="desktop"/>
