a27e6cd62054e8178556bb07d1c3fe58124da7c4
Richard Calvert-Smith <43348552+nikeforos-com@users.noreply.github.com>
2024-04-25 09:33:23 +0200

    Update ConsultingPage.tsx

--- src/UI/SimplePages/ConsultingPage.tsx
+++ src/UI/SimplePages/ConsultingPage.tsx
@@ -8,7 +8,7 @@ export function ConsultingPage() {
         <div>
           <h1>Consulting Services</h1>
           <p>There are more than 200 GRC solutions available from €100 to €100 000 per month, with new options coming out every couple of months.</p>
-          <p>Our extensive research can help you choose the right solution for your company by finding the right mix of homegrown and professional
+          <p>Our extensive research can help you choose the right solution for your company by finding the right mix of professional
             solutions for Risk, Compliance, Operations, Vendors, Contracts.</p>
           <p>We'll find the right solution for you. Send an email to <span className="underlined disappears">grace@chelsea.se</span> and let's
             setup a call!</p>
